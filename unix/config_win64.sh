629bd98108f9f659882cbb216e04d1aa547aea85
Kristian Kramer <kristian@beyonddata.llc>
2019-09-17 06:44:10 -0600

    Update config_win64.sh

--- unix/config_win64.sh
+++ unix/config_win64.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+logfile=configwin.log
+
+printf "-------------------- BEGIN LOGFILE --------------------" >> $logfile
+
+usage="$(basename "$0") Usage: [-h] -- script to install the required dependencies and build Beyondcoin for Ubuntu
+where:
+    -h show this help text
+"
+
+while getopts ':h' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+  esac
+done
+
 function pause(){
     read -p "$*"
 }
@@ -21,7 +38,7 @@ Donations [BYND]: BT8UTx2HjJmtY99Fm748aBjSdKedJfWwnQ
 pause "Press [Enter] to continue or [CTRL+C] to quit..."
 
 #add Bitcoin repository
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y >> $logfile
 
 #shared libraries and dependencies
 sudo apt-get install \
@@ -97,55 +114,55 @@ sudo apt-get install \
     python-cheetah \
     parted \
     kpartx \
-    bridge-utils -y
+    bridge-utils -y >> $logfile
 
-sudo apt-get update -y
+sudo apt-get update -y >> $logfile
 
 #install BerkelyDB for wallet support
-sudo wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c
+sudo wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz >> $logfile
+echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef  db-4.8.30.NC.tar.gz' | sha256sum -c >> $logfile
 
-sudo tar -xvf db-4.8.30.NC.tar.gz
+sudo tar -xvf db-4.8.30.NC.tar.gz >> $logfile
 cd db-4.8.30.NC/build_unix
 sudo mkdir -p build
 BDB_PREFIX=$(pwd)/build
 sudo ../dist/configure --disable-shared --enable-cxx --with-pic --prefix=$BDB_PREFIX
-sudo make install
+sudo make install >> $logfile
 cd ../..
 
 #install wm-builder for compiling
-sudo wget http://archive.ubuntu.com/ubuntu/pool/universe/v/vm-builder/vm-builder_0.12.4+bzr494.orig.tar.gz
-echo "76cbf8c52c391160b2641e7120dbade5afded713afaa6032f733a261f13e6a8e  vm-builder_0.12.4+bzr494.orig.tar.gz" | sha256sum -c
-sudo tar -zxvf vm-builder_0.12.4+bzr494.orig.tar.gz
+sudo wget http://archive.ubuntu.com/ubuntu/pool/universe/v/vm-builder/vm-builder_0.12.4+bzr494.orig.tar.gz >> $logfile
+echo "76cbf8c52c391160b2641e7120dbade5afded713afaa6032f733a261f13e6a8e  vm-builder_0.12.4+bzr494.orig.tar.gz" | sha256sum -c >> $logfile
+sudo tar -zxvf vm-builder_0.12.4+bzr494.orig.tar.gz >> $logfile
 cd vm-builder-0.12.4+bzr494
-sudo python setup.py install
+sudo python setup.py install >> $logfile
 cd ..
 
 #install and setup gitian-builder for cross compilation
-sudo git clone https://github.com/devrandom/gitian-builder.git
+sudo git clone https://github.com/devrandom/gitian-builder.git >> $logfile
 sudo chmod -R 777 gitian-builder
 cd gitian-builder
-sudo bin/make-base-vm --lxc --arch amd64 --suite precise
+sudo bin/make-base-vm --lxc --arch amd64 --suite precise >> $logfile
 sudo mkdir -p inputs;
 cd inputs/
 
 #install inputs for gitian-builder
-sudo wget 'http://miniupnp.free.fr/files/download.php?file=miniupnpc-1.9.20140401.tar.gz' -O 'miniupnpc-1.9.20140401.tar.gz'
-sudo wget 'https://www.openssl.org/source/openssl-1.0.1k.tar.gz'
-sudo wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
-sudo wget 'https://www.zlib.net/fossils/zlib-1.2.8.tar.gz'
-sudo wget 'ftp://ftp.simplesystems.org/pub/libpng/png/src/history/libpng16/libpng-1.6.8.tar.gz'
-sudo wget 'http://fukuchi.org/works/qrencode/qrencode-3.4.3.tar.bz2'
-sudo wget 'http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2'
-sudo wget 'https://download.qt.io/archive/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz'
-sudo wget 'https://svn.boost.org/trac/boost/raw-attachment/ticket/7262/boost-mingw.patch' -O boost-mingw-gas-cross-compile-2013-03-03.patch
+sudo wget 'http://miniupnp.free.fr/files/download.php?file=miniupnpc-1.9.20140401.tar.gz' -O 'miniupnpc-1.9.20140401.tar.gz' >> $logfile
+sudo wget 'https://www.openssl.org/source/openssl-1.0.1k.tar.gz' >> $logfile
+sudo wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz' >> $logfile
+sudo wget 'https://www.zlib.net/fossils/zlib-1.2.8.tar.gz' >> $logfile
+sudo wget 'ftp://ftp.simplesystems.org/pub/libpng/png/src/history/libpng16/libpng-1.6.8.tar.gz' >> $logfile
+sudo wget 'http://fukuchi.org/works/qrencode/qrencode-3.4.3.tar.bz2' >> $logfile
+sudo wget 'http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2' >> $logfile
+sudo wget 'https://download.qt.io/archive/qt/4.8/4.8.5/qt-everywhere-opensource-src-4.8.5.tar.gz' >> $logfile
+sudo wget 'https://svn.boost.org/trac/boost/raw-attachment/ticket/7262/boost-mingw.patch' -O boost-mingw-gas-cross-compile-2013-03-03.patch >> $logfile
 
 #download Beyondcoin source code
-sudo git clone https://github.com/Tech1k/beyondcoin.git
+sudo git clone https://github.com/Tech1k/beyondcoin.git >> $logfile
 sudo chmod -R 777 beyondcoin
 
-sudo apt-get upgrade -y
-sudo apt-get update -y
+sudo apt-get upgrade -y >> $logfile
+sudo apt-get update -y >> $logfile
 
 echo "The dependencies required to build Beyondcoin for Windows 10 on Ubuntu 18.04 have successfully been installed."
 read -n 1 -s -r -p "Press any key to continue..."
